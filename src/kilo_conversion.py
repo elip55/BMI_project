f3c44946f1279faeba7eb30231bbd709476a1de8
Eli Pacheco <eli.pacheco55@gmail.com>
2021-03-15 14:20:14 -0600

    Added stuff

--- src/kilo_conversion.py
+++ src/kilo_conversion.py
@@ -4,7 +4,7 @@
 def kilo_conversion(): # Function to convert kilos to lbs, since Americans are more comfortable with lbs 
     print("And now, lets get started on conversions, if needed.")
     answer = input("Do you need to convert kilos to lbs? y/n ")
-    if answer == 'y' or answer =='Y':
+    if answer == 'y' or answer ==' y':
         weight_kg = float(input("Please input your weight in kg "))
         weight_lbs = round(weight_kg*2.20462, 4)
         print(f"Your weight in lbs = {weight_lbs}")
