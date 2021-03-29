1d77c7a19e34a1d690b705bc99dd0329ace85597
Eli Pacheco <eli.pacheco55@gmail.com>
2021-03-29 17:53:56 -0600

    Added some stuffs

--- src/calculations.py
+++ src/calculations.py
@@ -1,7 +1,5 @@
 
 from .intro import goal
-from tqdm import tqdm
-import plotly.graph_objects as go
 
 def calculations(): # Final calculations 
 
@@ -12,18 +10,13 @@ def calculations(): # Final calculations
     healthy_BMI_min = 18
     healthy_BMI_max= 24
 
-    fig = go.Figure(data=[go.Table(header=dict(values=['Healthy BMI min', 'Healthy BMI max', 'Your BMI']),
-                 cells=dict(values=[[healthy_BMI_min], [healthy_BMI_max], [BMI]]))
-                     ])
-    for i in tqdm(range(100)):
-        pass
-    fig.show()
-
+    print(f"Your BMI: {BMI}")
+    print("-------------------")
 
     if BMI <= healthy_BMI_max and BMI >= healthy_BMI_min:
         print("Congrats, you are at a healthy BMI!")
     else:
-        print(f"\n Remember, a healthy BMI is between {healthy_BMI_min} and {healthy_BMI_max}! You have some work to do!")
+        print(f"Remember, a healthy BMI is between {healthy_BMI_min} and {healthy_BMI_max}! You have some work to do!")
     if weight > goal: 
         print("-------------------")
         print(f"You haven't reached your weight goal of {goal}.")
--- src/intro.py
+++ src/intro.py
@@ -1,4 +1,9 @@
 
 
 print("This is a simple program to find your goal weight and BMI.")
-goal = float(input("Before we start calculating, what is your goal weight, in lbs?"))
+try:
+    goal = float(input("Before we start calculating, what is your goal weight, in lbs?"))
+except:
+    print("Not a valid input, try again.")
+finally: 
+    goal = float(input("Before we start calculating, what is your goal weight, in lbs?"))
