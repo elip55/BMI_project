ddd7d963ad1c2a28e168bcd63e8739cd5747bc2e
Eli Pacheco <eli.pacheco55@gmail.com>
2021-03-29 18:12:00 -0600

    Added colors

--- src/intro.py
+++ src/intro.py
@@ -3,6 +3,9 @@
 print("This is a simple program to find your goal weight and BMI.")
 
 # Attain the user goal to congratulate them when they reach their goal
-goal = float(input("Before we start calculating, what is your goal weight, in lbs?"))
+try:
+    goal = float(input("Before we start calculating, what is your goal weight, in lbs?"))
+except: 
+    print("INVALID: Goal weight should be an integer, please run the program again.")
 
 
