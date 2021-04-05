6e5f57b0579b693c7458c6b152ddd01931efdfa4
Eli Pacheco <eli.pacheco55@gmail.com>
2021-04-05 17:58:22 -0600

    BMI goal

--- src/calculations.py
+++ src/calculations.py
@@ -1,6 +1,6 @@
 
 from .colors import bcolors
-from .intro import goal
+from .intro import goal_weight, goal_BMI
 import csv
 
 # Final calculations from gathered data
@@ -22,13 +22,13 @@ def calculations():
         print(bcolors.OKBLUE + "\n Congrats, you are at a healthy BMI!" + bcolors.ENDC)
     else:
         print(bcolors.FAIL + f"\n Remember, a healthy BMI is between {healthy_BMI_min} and {healthy_BMI_max}! You have some work to do!" + bcolors.ENDC)
-    if weight > goal: 
+    if weight > goal_weight: 
         print("-------------------")
-        print(bcolors.FAIL + f"\n You haven't reached your weight goal of {goal}." + bcolors.ENDC)
+        print(bcolors.FAIL + f"\n You haven't reached your weight goal of {goal_weight}." + bcolors.ENDC)
         print("-------------------")
-    elif weight <= goal:
+    elif weight <= goal_weight:
         print("-------------------")
-        print(bcolors.OKBLUE + f"\n Congrats!  You have reached your weight goal of {goal}!" + bcolors.ENDC)
+        print(bcolors.OKBLUE + f"\n Congrats!  You have reached your weight goal of {goal_weight}!" + bcolors.ENDC)
         print("-------------------")
     
     # This option will save an excel formatted file for records
@@ -37,7 +37,7 @@ def calculations():
         rowlist = [[" ", "WEIGHT", "BMI"],
                 ["LOW", " ", healthy_BMI_min],
                 ["HIGH", " ", healthy_BMI_max],
-                ["GOAL", goal, " "],
+                ["GOAL", goal_weight, goal_BMI],
                 ["ACTUAL", weight, BMI]]
                 
         with open('results.csv', 'w') as csvfile: 
--- src/intro.py
+++ src/intro.py
@@ -1,10 +1,11 @@
 from .colors import bcolors
 
-print("This is a simple program to find your goal weight and BMI.")
+print("This is a simple program to find your goal_weight weight and BMI.")
 
-# Attain the user goal to congratulate them when they reach their goal
+# Attain the users goals to congratulate them when they reach their goals
 try:
-    goal = float(input("Before we start calculating, what is your goal weight, in lbs?"))
+    goal_weight = float(input("Before we start calculating, what is your goal_weight weight, in lbs?"))
+    goal_BMI = float(input("What is your goal BMI?"))
 except: 
     print(bcolors.FAIL + "INVALID: Goal weight should be an integer, please run the program again." + bcolors.ENDC)
 
